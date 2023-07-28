c8c212c99c739e84f35c98d62a9f68d2dc68caca
Al <al@Als-MacBook-Pro.local>
2023-07-28 10:10:14 -0700

    first commit

--- components/meet/spotlight/IndividualAbout.tsx
+++ components/meet/spotlight/IndividualAbout.tsx
@@ -13,7 +13,6 @@ export default function IndividualAbout({ imageType }: Props): ReactElement {
 			<div className={styles.image}>
 				<ProfileImage imageType={imageType} image="https://images.unsplash.com/photo-1589729132389-8f0e0b55b91e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fGhlYWRzaG90fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />
 			</div>
-
 			<h4>Jane Doe</h4>
 			<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur mollitia sequi cupiditate commodi. Mollitia, quos sit reprehenderit molestias cumque perferendis alias explicabo maiores accusamus sequi, quisquam commodi ratione nemo odit. Lorem ipsum dolor sit, amet consectetur adipisicing elit. </p>
 		</div>
